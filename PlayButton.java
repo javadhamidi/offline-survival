084a37553f5b2e3a5ec78b381d9fc99a570cd817
Javad Hamidi <JahmHV@gmail.com>
2018-09-01 16:24:11 +0930

    button pressable?

--- PlayButton.java
+++ PlayButton.java
@@ -8,12 +8,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class PlayButton extends Actor
 {
+    
+    
     /**
      * Act - do whatever the PlayButton wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+        if (Greenfoot.mouseClicked(this)) 
+        {
+            PlayScreen ps = new PlayScreen();
+            Greenfoot.setWorld(ps);
+        }
+        
+        if (Greenfoot.mousePressed(this)) 
+        {
+            GreenfootImage scr = new GreenfootImage("home_play_button_pressed.png");
+            setImage(scr);
+        }
     }    
 }
